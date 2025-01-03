9e8670ca85351977f73e4f4d0d2f28f114ffb5a5
muhammad9970 <muhammad9970>
2025-01-03 12:09:39 +0000

    changes

--- currency_converter.py
+++ currency_converter.py
@@ -1,10 +1,3 @@
-exchange_rates = {
-    "USD": { "EUR": 0.85, "CAD": 1.25 },
-    "EUR": { "USD": 1.18, "CAD": 1.47 },
-    "CAD": { "USD": 0.80, "EUR": 1.47 },
-}
-currencies = ("USD", "EUR", "CAD")
-
 def get_amount():
     while True:
         try:
@@ -15,32 +8,30 @@ def get_amount():
         except ValueError:
             print("Invalid amount!")
 
-def get_source_currency():
+def get_currency(label):
+    currencies = ("USD", "EUR", "CAD")
     while True:
-        source_currency = input("Source currency (USD/EUR/CAD): ").upper()
-        if source_currency not in currencies:
+        currency = input(f"{label} currency (USD/EUR/CAD): ").upper()
+        if currency not in currencies:
             print("Invalid currency!")
         else:
-            return source_currency
+            return currency
 
-def get_target_currency():
-    while True:
-        target_currency = input("Target currency (USD/EUR/CAD): ").upper()
-        if target_currency not in currencies:
-            print("Invalid currency!")
-        else:
-            return target_currency
 
-def convert_currency():
-    source_currency = get_source_currency()
-    target_currency = get_target_currency()
-    amount = get_amount()
+def convert(amount, source_currency, target_currency):
+    exchange_rates = {
+    "USD": { "EUR": 0.85, "CAD": 1.25 },
+    "EUR": { "USD": 1.18, "CAD": 1.47 },
+    "CAD": { "USD": 0.80, "EUR": 1.47 },
+    }
 
     if source_currency == target_currency:
-        converted_amount = amount
-    else:
-        converted_amount = amount * exchange_rates[source_currency][target_currency]
+        return amount
+    return amount * exchange_rates[source_currency][target_currency]
 
-    print(f"{amount} {source_currency} is equal to {converted_amount:.2f}")
+amount = get_amount()
+source_currency = get_currency('Source')
+target_currency = get_currency('Target')
 
-convert_currency()
+converted_currency = convert(amount, source_currency, target_currency)
+print(f"{amount} {source_currency} is equal to {converted_currency:.2f}")

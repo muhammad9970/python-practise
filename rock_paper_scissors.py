16a86db0af0b3ebc8afd99626453fa1d598709ba
muhammad9970 <muhammad9970>
2025-01-03 11:46:10 +0000

    practise project

---
+++ rock_paper_scissors.py
@@ -0,0 +1,54 @@
+import random
+
+
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+
+emojis = {
+    ROCK : "🪨",
+    PAPER : "📰",
+    SCISSORS : "✂️"
+}
+choices = tuple(emojis.keys())
+
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, Paper or Scissors? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
+
+def display_choices(user_choice, computer_choice):
+    print(f"You chose {emojis[user_choice]}")
+    print(f"computer chose {emojis[computer_choice]}")
+
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("Tie")
+    elif (
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == SCISSORS and computer_choice == PAPER) or
+        (user_choice  == PAPER and computer_choice == ROCK)):
+        print("You win")
+    else:
+        print("You lose")
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+        should_continue = input("Continue? (y/n): ").lower()
+
+        if should_continue == "n":
+            break
+
+if __name__ == '__main__':
+    play_game()
